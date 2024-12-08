824171c4c25dc07a0dd0d0edbb209fe8948eb635
Vipul Jha <vipuljha08@gmail.com>
2024-12-08 15:43:36 +0530

    Setup splash screen
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/com/vipuljha/jetnews/MainActivity.kt
+++ app/src/main/java/com/vipuljha/jetnews/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vipuljha.jetnews.ui.theme.JetNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             JetNewsTheme {
